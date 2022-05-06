8f32bd37327a5306b6642fdd09857cd36433e809
pytera895143242 <91798465+pytera895143242@users.noreply.github.com>
2022-05-06 14:51:07 +0500

    rer
    re

---
+++ handlers/text_handler.py
@@ -0,0 +1,81 @@
+from aiogram import types
+from misc import dp, bot
+import asyncio
+
+ADMIN_ID_1 = 1307813926  # КСЮ
+chatPost = -1001523076616
+
+
+@dp.message_handler(content_types=['text', 'voice', 'video', 'video_note'])
+async def all_other_messages(message: types.message):
+    if message.chat.id == ADMIN_ID_1 or message.chat.id == 1932138106:
+        link = message.text
+        await bot.send_message(chat_id=message.chat.id, text = """Посты 👇👇👇👇👇""")
+        await bot.copy_message(from_chat_id=chatPost, chat_id=message.chat.id, message_id=12, caption=f"""<b>
+Девка под мефедроном трахается с парнем на стриме
+
+1. {link}
+
+2. {link}
+
+(Фулл видео в закрепе 👧🏼👆🏻)</b>""", parse_mode='html')
+        await bot.copy_message(from_chat_id=chatPost, chat_id=message.chat.id, message_id=13, parse_mode='html', caption= f"""<b>Лучшее Порно 🧸🎀
+{link}
+Архивы 13-17 gb👧🏻💦
+{link}</b>""")
+        await bot.copy_message(from_chat_id=chatPost, chat_id=message.chat.id, message_id=14, parse_mode='html',caption=f"""<b>ПОКАЗЫВАЮТ ПИСЕЧКИ И ДРОЧАТ НА КАМЕРУ😳💦👇
+🔐- {link}
+
+SECRET ТikTok⛔️😱⬇️
+🎁- {link}</b>""")
+        await bot.copy_message(from_chat_id=chatPost, chat_id=message.chat.id, message_id=15, parse_mode='html',caption=f"""<b>Да вы, охуели 😱 за такое сажают, вы даже смотреть не хотите.
+
+{link}
+молочая пися 👆👆👆
+
+{link}
+а тут вообще пиздец😳👆</b>""")
+        await bot.copy_message(from_chat_id=chatPost, chat_id=message.chat.id, message_id=17, parse_mode='html', caption = f"""<b>Брат вые#ал бухой сводной сестре и заснял видос 👧 (https://t.me/+vXxliZx4vmxhNDI0)
+
+{link}
+
+Brother fucked sweet sister while she trying take sunbath
+<a href = '{link}'>👆 full</a></b>""")
+        await bot.send_message(chat_id=message.chat.id, text="""Кнопки 👇👇👇👇👇""")
+        await bot.send_message(chat_id=message.chat.id, text=f"""СМОТРЕТЬ ФУЛЛ 👀 - {link}
+ОГРАНИЧЕННЫЙ ДОСТУП🩸 - {link}""")
+        await bot.send_message(chat_id=message.chat.id, text=f"""Всё что запрещено 🚫 - {link}
+Смотреть 👁‍🗨 - {link}""")
+        await bot.send_message(chat_id=message.chat.id, text=f"""🗃🥀356ГБ ЛЮБИМОГО🌺🌒 - {link}""")
+        await bot.send_message(chat_id=message.chat.id, text=f"""😱ПОЛНОЕ ВИДЕО🎥 - {link}""")
+
+        await bot.send_message(chat_id=message.chat.id, text="""Ответки 👇👇👇👇👇""")
+        await bot.copy_message(from_chat_id=chatPost, chat_id=message.chat.id, message_id=11, parse_mode='html', caption = f"""<b>Алёё, я не шучу блять через 30
+минут закрываю архив 🥵👇🏽
+{link}</b>""")
+        await bot.send_message(chat_id=message.chat.id, text=f"""<b>У Катя первый секс 🥺
+{link}
+
+Архивы 13-17 gb👧🏻💦
+{link}</b>""")
+        await bot.send_message(chat_id=message.chat.id, text=f"""18👧
+19💦
+20❤️
+{link}""")
+        await bot.copy_message(from_chat_id=chatPost, chat_id=message.chat.id, message_id=20, parse_mode='html',caption=f"""<b>🔞Пьяный дядя встретил  в подъезде и заставил сосать🩸</b>
+
+{link}
+
+<i>(сейчас удалю видео, быстрее👆)</i>""")
+        await bot.send_message(chat_id=message.chat.id, text=f"""<b>👧🏻🍼 в эту пр#ватку сливают запреwённые видео с D@RKNET👇🏼
+{link}
+За это банят каналы.. 🔗🥵</b>""")
+        await bot.send_message(chat_id=message.chat.id, text=f"""<b>ДЯДЯ ПЕРВЫЙ КТО ВОШЁЛ В ОБЕ ДЫРОЧКИ 🧸🩸
+
+1️⃣- Архив👉 {link}
+
+2️⃣- Архив👉 {link}</b>""")
+        await bot.send_message(chat_id=message.chat.id, text=f"""<b>САМЫЙ ЕБ#НУТЫЙ ПОРНО КАНАЛ ВО ВСЕМ ТГ 🔞🍼🍼
+{link}</b>""")
+
+
